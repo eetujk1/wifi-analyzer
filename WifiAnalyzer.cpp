eb578e3905f17d811688761fcca74a0aeaec03e1
eetujk1 <eetukanniainen123@gmail.com>
2026-08-17 15:15:48 +0300

    Initial commit

---
+++ WifiAnalyzer.cpp
@@ -0,0 +1,14 @@
+﻿// WifiAnalyzer.cpp : Defines the entry point for the application.
+//
+
+#include "WifiAnalyzer.h"
+#include <windows.h>
+#include <wlanapi.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Wifi-analyzer" << endl;
+	return 0;
+}
