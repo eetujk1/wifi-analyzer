be77423db7fe3086ad62fbd147bb71dc0257422a
eetujk1 <eetukanniainen123@gmail.com>
2026-08-17 17:28:36 +0300

    add WLAN interface enumeration

--- WifiAnalyzer.cpp
+++ WifiAnalyzer.cpp
@@ -1,7 +1,7 @@
-﻿
-#include "WifiAnalyzer.h"
+﻿#include "WifiAnalyzer.h"
 #include <windows.h>
 #include <wlanapi.h>
+#include <iostream>
 
 using namespace std;
 
@@ -9,16 +9,42 @@ int main()
 {
 	HANDLE wlanhandle = nullptr;
 	DWORD negotiatedVersion = 0;
-	
+
 	DWORD result = WlanOpenHandle(2, nullptr, &negotiatedVersion, &wlanhandle);
 
 	if (result != ERROR_SUCCESS) {
 		std::cout << "Error: " << result << std::endl;
-
 		return 1;
 	}
 
 	std::cout << "Wlan API connected successfully!" << std::endl;
+
+	PVOID pReserved = nullptr;
+	PWLAN_INTERFACE_INFO_LIST pInterfaceList = nullptr;
+
+	result = WlanEnumInterfaces(
+		wlanhandle,
+		pReserved,
+		&pInterfaceList
+	);
+
+	if (result != ERROR_SUCCESS) {
+		std::cout << "Error: Failed to enumerate WLAN interfaces. Code: " << result << std::endl;
+		WlanCloseHandle(wlanhandle, nullptr);
+		return 1;
+	}
+
+	std::cout << "WLAN interfaces enumerated successfully!" << std::endl;
+
+	if (pInterfaceList != nullptr) {
+		WlanFreeMemory(pInterfaceList);
+		pInterfaceList = nullptr;
+	}
+
+	if (wlanhandle != nullptr) {
+		WlanCloseHandle(wlanhandle, nullptr);
+		wlanhandle = nullptr;
+	}
 
 	return 0;
 }
